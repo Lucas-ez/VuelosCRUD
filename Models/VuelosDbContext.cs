e3af6724823095a8520d674404deacb0dca3cca8
Lucas <lucasezequiel237@gmail.com>
2023-04-15 15:33:38 -0300

    Add db conection

---
+++ Models/VuelosDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VuelosCRUD.Models
+{
+  public class VuelosDbContext : DbContext
+  {
+    public VuelosDbContext(DbContextOptions<VuelosDbContext> options) : base(options) { }
+
+    public DbSet<Vuelo> Vuelos { get; set; }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using VuelosCRUD.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Conexión con la db
+builder.Services.AddDbContext<VuelosDbContext>(
+  opciones => opciones.UseSqlServer(builder.Configuration.GetConnectionString("DbConectionString")
+));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,9 +16,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
