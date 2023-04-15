7fa86dd53931cbc500f1a250d2cbbdbd4c9173a0
Lucas <lucasezequiel237@gmail.com>
2023-04-15 15:14:04 -0300

    Add Vuelo Model

---
+++ Models/Vuelo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VuelosCRUD.Models
+{
+	public class Vuelo
+	{
+		public int Id { get; set; }
+
+		[StringLength(8)]
+		[Display(Name = "Vuelo")]
+		public string NumeroVuelo { get; set; }
+
+		[Display(Name = "Horario LLegada")]
+		public DateTime HorarioLLegada { get; set; }
+
+		[StringLength(255)]
+		[Display(Name = "Línea Aerea")]
+		public string LineaAerea { get; set; }
+		public bool Demorado { get; set; }
+	}
+}
