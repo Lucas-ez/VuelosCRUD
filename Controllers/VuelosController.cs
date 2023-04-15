f92f9171ee383eb3c193a3150e8c76d3c4fa769a
Lucas <lucasezequiel237@gmail.com>
2023-04-15 15:26:05 -0300

    Add Vuelos views

--- Controllers/VuelosController.cs
+++ Controllers/VuelosController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VuelosCRUD.Models;
 
 namespace VuelosCRUD.Controllers
 {
-	public class VuelosController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+  public class VuelosController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Create()
+    {
+      var vuelo = new Vuelo();
+
+      return View("Form", vuelo);
+    }
+    public IActionResult Update()
+    {
+      var vuelo = new Vuelo();
+
+      return View("Form", vuelo);
+    }
+    public IActionResult Delete()
+    {
+      return View();
+    }
+  }
 }
